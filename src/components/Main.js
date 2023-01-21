83fb6d9e0094d761c7b48d38e81bf985da7d5a78
hcustod <111096105+hcustod@users.noreply.github.com>
2023-01-20 23:34:00 -0500

    first

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="main">
+      <h2>Welcome to my website</h2>
+      <br />
+      <p>This is the main content area of my website.</p>
+    </div>
+  );
+}
+
+export default Main;
